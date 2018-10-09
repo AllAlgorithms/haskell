9b9106847328e3b86346183d4339b06e0d73f164
Saxon Jensen <saxon.jensen@healthengine.com.au>
2018-10-09 16:22:29 +0800

    Much more general fibonacci function

--- math/Fibonacci.hs
+++ math/Fibonacci.hs
@@ -1,10 +1,4 @@
-fibonacci :: (Integral a) => a -> [a]
-fibonacci 1 = [1]
-fibonacci 2 = [1, 1]
-fibonacci x
-    | x < 1  = error "Fibonacci sequence length must be >= 1"
-    | otherwise = n:fibs
-        where
-            fibs = fibonacci (x - 1)
-            n = sum $ take 2 fibs
-
+fibonacci :: Integer -> Integer
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci x = fibonacci (x - 1) + fibonacci (x - 2)
