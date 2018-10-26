29a0931017380f683728260faa598be2788d59b0
renanpvaz <renan.piresvz@gmail.com>
2018-10-26 19:49:23 -0300

    Improve readability

--- sorting/insertionSort.hs
+++ sorting/insertionSort.hs
@@ -1,12 +1,10 @@
 insert :: Ord a => a -> [a] -> [a]
 insert elem [] = [elem]
 insert elem (x:[]) = min elem x:max elem x:[]
-insert elem (x:y:xs) =
-  if elem > x && elem < y
-    then x:elem:y:xs
-    else if elem < x && elem < y
-    then elem:x:y:xs
-    else x:insert elem (y:xs)
+insert elem (x:y:xs)
+  | elem > x && elem < y = x:elem:y:xs
+  | elem < x && elem < y = elem:x:y:xs 
+  | otherwise            = x:insert elem (y:xs)
 
 
 insertionSort :: Ord a => [a] -> [a]
