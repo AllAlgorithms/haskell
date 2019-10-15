4e4479c7df1163e9f9d6b28e4cebfc4fbeba99ce
Steve <sameyer17@gmail.com>
2019-10-15 18:16:34 -0400

    added a new line at end of file

--- sorting/selectionSort.hs
+++ sorting/selectionSort.hs
@@ -4,3 +4,4 @@ selectionSort :: Ord a => [a] -> [a]
 selectionSort [x] = [x]
 selectionSort xs = let minValue = minimum xs 
                     in minValue : selectionSort (delete minValue xs)
+                    
